f38ad6adfe684aecab7e7881deb48b8759093075
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-08-03 18:07:42 -0700

    Added decorator for commands and a command registry.

--- hyasynth/app/shell/command.py
+++ hyasynth/app/shell/command.py
@@ -1,12 +1,40 @@
+from carapace.app import registry
+from carapace.app.shell import base
+from carapace.sdk import registry
+
 from hyasynth.app.sc import client
 
 
-# XXX add a decorator that can be used by methods in this class to say
-# whether a given method is to be used in the shell as a command; will probably
-# update a dict/list on the class that holds all command methods.
+config = registry.getConfig()
+
+
+class CommandRegistry(object):
+    """
+    """
+    registry = []
+
+    def add(self, func):
+        """
+        """
+        self.registry.append(func.func_name)
+
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+
+
+commands = CommandRegistry()
+
+
 class BaseAPI(object):
     """
     """
+    _commands = commands
+
+    def getCommands(self):
+        """
+        """
+        return self._commands.registry
 
 
 class ShellAPI(BaseAPI):
@@ -40,6 +68,11 @@ class ShellAPI(BaseAPI):
     def getAppData(self):
         return pprint(self.appData)
 
+    @commands.add
+    def app(self):
+        return self.getAppData()
+
+    @commands.add
     def ls(self):
         """
         List the objects in the current namespace, in alphabetical order.
@@ -58,20 +91,35 @@ class ShellAPI(BaseAPI):
                 info = "%s.%s" % (value.__module__, value.__name__)
             elif type(value).__name__ == "instance":
                 info = "%s.%s" % (value.__module__, value.__class__.__name__)
-            else:
+            elif hasattr(value, "im_class"):
                 info = "%s.%s.%s" % (
                     value.im_class.__module__, value.im_class.__name__, key)
+            elif hasattr(value, "__class__") and hasattr(value, "__module__"):
+                info = "%s.%s.%s" % (
+                    value.__module__, value.__class__.__name__, key)
+            else:
+                info = "<Unknown>"
             print "\t%s - %s" % (key.ljust(width), info)
 
+    @commands.add
     def banner(self):
         """
         Display the login banner and associated help or info.
         """
-        print base.renderBanner(help=BANNER_HELP)
+        banner = base.renderBanner(help=config.ssh.banner_help)
+        print banner.replace("{{WELCOME}}", config.ssh.welcome)
 
+    @commands.add
+    def welcome(self):
+        """
+        """
+        print config.ssh.banner.replace("{{WELCOME}}", config.ssh.welcome)
+
+    @commands.add
     def clear(self):
         self.terminal.reset()
 
+    @commands.add
     def quit(self):
         self.terminal.loseConnection()
 
@@ -79,12 +127,15 @@ class ShellAPI(BaseAPI):
 class OSCAPI(BaseAPI):
     """
     """
+    @commands.add
     def send(self, *args, **kwargs):
         return client.send(*args, **kwargs)
 
+    @commands.add
     def status(self, *args, **kwargs):
         return client.status(*args, **kwargs)
 
+    @commands.add
     def server_status(self, *args, **kwargs):
         return client.server_status(*args, **kwargs)
 
